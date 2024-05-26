f80ca0afbfd52e7115ab3b2eb15704970ea026f7
yupan <2429862664@qq.com>
2024-05-26 23:08:48 +0800

    feat: 初始化项目

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/cn/yp/dynamic/thread/pool/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,9 @@
+package cn.yp.dynamic.thread.pool.config;
+
+/**
+ * @author yp
+ * @description 动态线程池配置类
+ * @create 2024/5/26 22:50
+ */
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/test/java/cn/yp/dynamic/thread/pool/ApiTest.java
@@ -0,0 +1,9 @@
+package cn.yp.dynamic.thread.pool;
+
+/**
+ * @author yp
+ * @description 单元测试
+ * @create 2024/5/26 22:53
+ */
+public class ApiTest {
+}
